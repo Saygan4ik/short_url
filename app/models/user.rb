a86d8da278c5c8a72ea053592319b771287379aa
Saygan4ik <sergey.saygan@gmail.com>
2018-01-11 14:39:28 +0600

    add token for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_secure_token
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable
   has_many :urls, dependent: :destroy
