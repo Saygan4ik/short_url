c0cc80cb35ccdadc2e441aed812b816619d9539b
Saygan4ik <sergey.saygan@gmail.com>
2018-01-11 13:44:56 +0600

    refactoring code in 'Url' model and controller

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'securerandom'
-
 class UrlsController < ApplicationController
   before_action :authenticate_user!
 
@@ -14,60 +12,30 @@ class UrlsController < ApplicationController
   end
 
   def show
-    @url = Url.find(params[:id])
-    if @url.user_id == current_user[:id]
-      @url
-    else
-      flash[:alert] = 'Access denied'
-      redirect_to action: :new
-    end
+    @url = current_user.urls.find(params[:id])
   end
 
   def create
-    if check_url_from_user?(params[:url][:initial_url])
-      flash[:notice] = 'This url is exists already'
-      redirect_to action: :new
+    @url = current_user.urls.create(url_params)
+    if @url.valid?
+      flash[:alert] = 'Short url created'
+      redirect_to @url
     else
-      params[:url][:short_url] = get_short_url
-      if @url = current_user.urls.create(url_params)
-        redirect_to @url
-      else
-        redirect_to action: :new
-      end
+      render :new
     end
   end
 
   def destroy
-    @url = Url.find(params[:id])
-    if @url.user_id == current_user[:id]
-      @url.destroy
-      flash[:notice] = 'Url deleted'
-    else
-      flash[:alert] = 'Access denied'
-    end
+    @url = current_user.urls.find(params[:id])
+    @url.destroy
+    flash[:notice] = 'Url deleted'
 
     redirect_to action: :index
   end
 
   private
 
-  def get_short_url
-    @short_url_all = Url.all.select('short_url')
-    loop do
-      @new_short_url = SecureRandom.hex(3)
-      unless @short_url_all.map(&:short_url).include?(@new_short_url.to_s)
-        break
-      end
-    end
-
-    @new_short_url
-  end
-
-  def check_url_from_user?(url)
-    current_user.urls.map { |item| item[:initial_url] }.include?(url)
-  end
-
   def url_params
-    params.require(:url).permit(:initial_url, :short_url)
+    params.require(:url).permit(:initial_url)
   end
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -2,4 +2,18 @@ class Url < ApplicationRecord
   belongs_to :user
   validates :short_url, uniqueness: true
   validates :initial_url, presence: true
+  validates :initial_url, uniqueness: { scope: :user_id }
+
+  before_validation :generate_short_url
+
+  private
+
+  def generate_short_url
+    short_url = SecureRandom.hex(3)
+    while Url.where(short_url: short_url).exists?
+      short_url = SecureRandom.hex(3)
+    end
+
+    self.short_url = short_url
+  end
 end
