55ce1a26efe2fcfa4eab809ce5f0c2a3408e456e
Saygan4ik <sergey.saygan@gmail.com>
2018-01-12 12:39:34 +0600

    add quantity limit for urls for each users

--- app/models/url.rb
+++ app/models/url.rb
@@ -5,6 +5,7 @@ class Url < ApplicationRecord
   validates :short_url, uniqueness: true
   validates :initial_url, presence: true
   validates :initial_url, uniqueness: { scope: :user_id }
+  validate :minimum_quantity_urls_for_user, on: :create
 
   before_validation :generate_short_url, on: :create
 
@@ -17,5 +18,10 @@ class Url < ApplicationRecord
     end
 
     self.short_url = short_url
+  end
+
+  def minimum_quantity_urls_for_user
+    errors.add(:user_id, 'Quantity urls greater then 10') if
+        Url.where(user_id: self.user_id).count >= 10
   end
 end
