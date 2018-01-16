3b84f5144949aeaaccca6a1f86e7795c3a224c60
Saygan4ik <sergey.saygan@gmail.com>
2018-01-16 11:59:34 +0600

    add method API for uploading files from a remote location

--- app/controllers/api/v1/userfiles_controller.rb
+++ app/controllers/api/v1/userfiles_controller.rb
@@ -7,7 +7,26 @@ module Api
 
       def index
         @userfiles = @user.userfiles.page(params[:page]).per(params[:per_page])
-        render json: @userfiles, serializer: PaginationSerializer, status: :ok
+        render json: @userfiles,
+               serializer: PaginationSerializer,
+               status: :ok
+      end
+
+      def create
+        @userfile = @user.userfiles.new(userfile_params)
+        if @userfile.save
+          render json: @userfile,
+                 status: :ok
+        else
+          render json: @userfile.errors.full_messages,
+                 status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def userfile_params
+        params.require(:userfile).permit(:remote_file_url)
       end
     end
   end
